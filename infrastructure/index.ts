8b744fe7a6efe9568ff18c48993cd6dc1f51bcb4
jatinhooda <69504400+jatinhooda@users.noreply.github.com>
2025-01-23 18:16:13 -0500

    Load the application config information

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi';
+
+const config = new pulumi.Config();
+const appPath = config.require('appPath');
+const prefixName = config.require('prefixName');
+const imageName = prefixName;
+const imageTag = config.require('imageTag');
+const containerPort = config.requireNumber('containerPort');
+const publicPort = config.requireNumber('publicPort');
+const cpu = config.requireNumber('cpu');
+const memory = config.requireNumber('memory');
